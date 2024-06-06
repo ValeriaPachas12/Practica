e0744273cce71fc6850ea988e550f3ef3afabda5
Valeria <valeleriapa226@gmail.com>
2024-06-06 13:24:18 -0500

    carga inicial

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author UPSJB
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1=6, num2=8, suma=0;
+        suma = num1+num2;
+        System.out.println("La suma es: "+suma);
+    }
+    
+}
